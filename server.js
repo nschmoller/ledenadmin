c693e69ab1e44d6fe488577fcbc2096e477cdf15
Niek Schmoller <niek.schmoller@helderberg.nl>
2012-01-23 21:03:14 +0100

    Initial commit.

---
+++ server.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+function handleRequest(req, res) {
+  var url = req.url;
+  if (url == '/') {
+    url = '/index.mustache';
+  }
+  var filePath = '.' + url;
+  path.exists(filePath, function (exists) {
+    if (exists) {
+      fs.readFile(filePath, function (err, data) {
+        if (err) {
+          res.writeHead(500);
+          res.end();
+        }
+        console.log("200: " + url);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data);
+      });
+    } else {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      res.end('File not found.');
+    }
+  });
+}
+
+http.createServer(function (req, res) {
+  handleRequest(req, res);
+}).listen(3000, "localhost");
+
+console.log("server running at http://localhost:3000");
