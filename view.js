74ecef79d945cfa4d17e223a957b71b1960654f5
Niek Schmoller <niek.schmoller@helderberg.nl>
2012-02-02 09:12:20 +0100

    First pieces of backbone.

--- view.js
+++ view.js
@@ -1,12 +1,12 @@
 var template = null;
 var data = null;
 
-$.get("leden.mustache", function (leden_template) {
+$.get("leden.mustache", function(leden_template) {
   template = leden_template;
   render();
 });
 
-$.getJSON("leden", function (leden_data) {
+$.getJSON("leden", function(leden_data) {
   data = leden_data;
   render();
 });
@@ -20,3 +20,51 @@ function render() {
     $('body').append(ledenlijst);
   });
 }
+
+$(function() {
+  window.Member = Backbone.Model.extend({
+  });
+
+  window.MemberCollection = Backbone.Collection.extend({
+    model: Member
+  });
+
+  window.Members = new MemberCollection;
+
+  window.MemberView = Backbone.View.extend({
+    className: 'member',
+    model: Member
+  });
+
+  window.AddMemberDialog = Backbone.View.extend({
+    events: {
+      'keypress new-member-name': 'saveOnEnter'
+    },
+    render: function() {
+      this.$el.html('<div class="add-member-dialog"><input type="text" id="new-member-name"></div>');
+      return this;
+    },
+    saveOnEnter: function(event) {
+      console.info("key pressed");
+      console.info(event);
+      if (event.keyCode === 13) {
+        Members.create({name: $('new-member-name').val()});
+      }
+    }
+  });
+
+  window.AppView = Backbone.View.extend({
+    el: $('body'),
+    events: {
+      'click #new_leden': 'addMemberDialog'
+    },
+    addMemberDialog: function(event) {
+      console.info('hier');
+      event.preventDefault();
+      var view = new AddMemberDialog();
+      $('body').append(view.render().el);
+    }
+  });
+
+  window.App = new AppView;
+});
